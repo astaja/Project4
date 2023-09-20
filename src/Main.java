0cc92ffba7dea87b500938d4292e3f774327e18d
astaja <asta.jakx@gmail.com>
2023-09-20 21:02:34 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,100 @@
+import javax.sound.midi.Soundbank;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        // 1 task
+        //  Sukurkite Funkciją kuri priima du int tipo kintamuosius. Juos susumuoja ir atspausdina.
+
+//        int a = 2;
+//        int b = 1;
+//        sum(a, b);
+//        sum(2, 1);
+
+        // 2 task
+        // Sukurkite Funkciją kuri vadinasi PISq.
+        // Funkcija gražina double tipo reikšmę. Reikšmė yra : 9.8596; Gautą reikšmę atspausdinkite.
+
+     //   System.out.println("Number is " + PISq());
+
+      // 3 task
+     //Sukurkite Funkciją kuri priima du int tipo kintamuosius. Funkcija gražina skaičių sandaugą.;
+     // Gautą reikšmę atspausdinkite.
+
+//        int c = 3;
+//        int d = 2;
+//        digits(c,d);
+//        int multi = digits(c,d);
+//        System.out.println(multi);
+//        System.out.println(digits(c,d));
+
+     // 4 task
+     //   Sukurkite Funkciją kuri priima int[] tipo kintamąį, prasuka ciklą
+     //  ir atspausdina kiekvieną skaičių.
+
+//        int [] symbols = {10,20,30,40,50};
+//        System.out.println(symbols);
+//        sequence(symbols);
+
+       // 5 task
+     // Sukurkite Funkciją kuri priima du int tipo kintamuosius, min ir max
+     // reikšmėms nustatyti ir sugeneruoja random int skaičių ir jį gražintų.
+
+//        int min = 5;
+//        int max = 10;
+//        values(min, max);
+//        System.out.println(values(min,max));
+
+        // 6 task
+//        Sukurkite Funkciją kuri sugeneruotų random int skaičių masyvą ir jį gražintų.
+//        Funkcija priima tris int tipo kintamuosius, min, max ir length reikšmėms nustatyti.
+
+        int min = 3;
+        int max = 9;
+        int length = 10;
+        int[] randomArr =
+
+
+
+    }
+//    public static void sum(int a, int b) {
+//        int total = a + b;
+//        System.out.println(total);
+//    }
+//    public static double PISq() {
+//        return 9.8596 ;
+//    }
+//    public static int digits (int c, int d) {
+//        int multi = c * d;
+//        return multi;
+//    }
+//    public static void sequence(int[] symbols) {
+//        for (int i = 0; i < symbols.length; i++) {
+//            System.out.println(symbols[i] + " ");
+//        }
+//        System.out.println();
+//    }
+//
+//        public static int values(int min, int max) {
+//        int random1 = min + (int) Math.round(Math.random() * (max - min));
+//        int random2 = min + (int) Math.round(Math.random() * (max - min));
+//        return random1 + random2;
+//      }
+
+          public static int[]
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
